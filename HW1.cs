02cc56cac399efdc2280542b48d0cf0e873dae58
EricStarks3347 <157085771+EricStarks3347@users.noreply.github.com>
2024-01-18 21:16:04 -0600

    Add files via upload

---
+++ HW1.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace ZZHelloWorld;
+
+
+class Program
+{
+  
+
+    static void Main(string[] args)
+    {
+        /* X value*/
+        double X = 2.5;
+        /*4X^2*/
+        double A = 4*X*X;
+        /*Y value*/
+        double Y =3.3;
+        /*3*Y value*/
+        double B = 3*Y;
+
+        
+        Console.WriteLine(A+B);
+    }
+
+   
+}
